3dd5d455486ae22ea6b4259005e113f0df239751
gautamkandpal <gkandpal33@gmail.com>
2024-10-23 18:06:23 +0530

    feat: added header.jsx and routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Dashboard from "./pages/Dashboard";
+import About from "./pages/About";
+import Header from "./components/Header";
+import Projects from "./pages/Projects";
+
 function App() {
   return (
-    <>
-      <h1 className="text-red-700">thought nest </h1>
-    </>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </Router>
   );
 }
 
